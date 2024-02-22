c699b55fa955852544748b0b42c6e754491e1428
masprog2022 <masprog2022@gmail.com>
2024-02-22 22:52:56 +0100

    feat:  save image to database

--- server/src/main/java/com/masprogtech/imageliteapi/application/images/ImageController.java
+++ server/src/main/java/com/masprogtech/imageliteapi/application/images/ImageController.java
@@ -1,7 +1,12 @@
 package com.masprogtech.imageliteapi.application.images;
 
 
+import com.masprogtech.imageliteapi.domain.entity.Image;
+import com.masprogtech.imageliteapi.domain.enumerated.ImageExtension;
+import com.masprogtech.imageliteapi.service.ImageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,21 +14,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 
 @RestController
 @RequestMapping("/v1/images")
 @Slf4j
+@RequiredArgsConstructor
 public class ImageController {
 
+    private final ImageService imageService;
     @PostMapping
     public ResponseEntity save(@RequestParam("file") MultipartFile file,
                                @RequestParam("name") String name,
-                               @RequestParam("tags") List<String> tags ){
+                               @RequestParam("tags") List<String> tags ) throws IOException {
         log.info("Imagem recebida: name: {}, size: {} ", file.getOriginalFilename(), file.getSize());
         log.info("Nome definido para a imagem: {}", name);
         log.info("Tags: {}", tags);
+        log.info("Media Type: {}", MediaType.valueOf(Objects.requireNonNull(file.getContentType())));
+
+
+        Image image = Image.builder()
+                .name(name)
+                .tags(String.join(",", tags)) // ["tag1","tag2"] salvo no banco tag1 tag2
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(Objects.requireNonNull(file.getContentType()))))
+                .file(file.getBytes())
+                .build();
+
+        imageService.save(image);
+
       return ResponseEntity.ok().build();
     }
 }
