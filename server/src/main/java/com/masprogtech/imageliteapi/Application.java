c0d98c765270f230a2e66600366034cd87078551
masprog2022 <masprog2022@gmail.com>
2024-02-22 22:08:55 +0100

    feat: add ImageRepository

--- server/src/main/java/com/masprogtech/imageliteapi/Application.java
+++ server/src/main/java/com/masprogtech/imageliteapi/Application.java
@@ -1,13 +1,32 @@
 package com.masprogtech.imageliteapi;
 
+import com.masprogtech.imageliteapi.domain.entity.Image;
+import com.masprogtech.imageliteapi.domain.enumerated.ImageExtension;
+import com.masprogtech.imageliteapi.infra.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application {
 
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired ImageRepository repository){
+      return args -> {
+		  Image image = Image.builder()
+				  .extension(ImageExtension.PNG)
+				  .name("myimage")
+				  .tags("teste")
+				  .size(1000L)
+				  .build();
+
+		  repository.save(image);
+	  };
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- server/src/main/java/com/masprogtech/imageliteapi/infra/repository/ImageRepository.java
+++ server/src/main/java/com/masprogtech/imageliteapi/infra/repository/ImageRepository.java
@@ -1,2 +1,8 @@
-package com.masprogtech.imageliteapi.infra.repository;public interface ImageRepository {
+package com.masprogtech.imageliteapi.infra.repository;
+
+import com.masprogtech.imageliteapi.domain.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, String> {
+
 }
