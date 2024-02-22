0da2ff93e0d34f2eda1b6a52f278ad6001b7eb70
masprog2022 <masprog2022@gmail.com>
2024-02-22 23:56:19 +0100

    feat: add optional getById

--- server/src/main/java/com/masprogtech/imageliteapi/service/ImageService.java
+++ server/src/main/java/com/masprogtech/imageliteapi/service/ImageService.java
@@ -2,7 +2,10 @@ package com.masprogtech.imageliteapi.service;
 
 import com.masprogtech.imageliteapi.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
 
     Image save(Image image);
+    Optional<Image> geById(String id);
 }
