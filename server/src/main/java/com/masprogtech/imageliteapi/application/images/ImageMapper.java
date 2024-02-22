c150ada331ae45a3cee85148de7522c9d7345192
masprog2022 <masprog2022@gmail.com>
2024-02-22 23:19:24 +0100

    feat: add ImageMapper

--- server/src/main/java/com/masprogtech/imageliteapi/application/images/ImageMapper.java
+++ server/src/main/java/com/masprogtech/imageliteapi/application/images/ImageMapper.java
@@ -1,2 +1,24 @@
-package com.masprogtech.imageliteapi.application.images;public class ImageMapper {
+package com.masprogtech.imageliteapi.application.images;
+
+import com.masprogtech.imageliteapi.domain.entity.Image;
+import com.masprogtech.imageliteapi.domain.enumerated.ImageExtension;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class ImageMapper {
+    public Image mapToImage(MultipartFile file, String name, List<String> tags) throws IOException {
+        return Image.builder()
+                .name(name)
+                .tags(String.join(",", tags)) // ["tag1","tag2"] salvo no banco tag1 tag2
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(Objects.requireNonNull(file.getContentType()))))
+                .file(file.getBytes())
+                .build();
+    }
 }
