0b187cb5364be04ea147a01eb2a50113be5c5767
masprog2022 <masprog2022@gmail.com>
2024-02-22 23:54:33 +0100

    feat: add getImage

--- server/src/main/java/com/masprogtech/imageliteapi/application/images/ImageController.java
+++ server/src/main/java/com/masprogtech/imageliteapi/application/images/ImageController.java
@@ -6,12 +6,11 @@ import com.masprogtech.imageliteapi.domain.enumerated.ImageExtension;
 import com.masprogtech.imageliteapi.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -42,6 +41,22 @@ public class ImageController {
         URI imageUri = buildImageURL(savedImage);
 
         return ResponseEntity.created(imageUri).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String id){
+      var possibleImage = imageService.geById(id);
+      if(possibleImage.isEmpty()){
+          return ResponseEntity.notFound().build();
+      }
+        var image = possibleImage.get();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(image.getExtension().getMediaType());
+        headers.setContentLength(image.getSize());
+        headers.setContentDispositionFormData("inline; filename=\"" + image.getFileName() + "\"", image.getFileName());
+
+        return new ResponseEntity<>(image.getFile(), headers, HttpStatus.OK);
     }
 
     private URI buildImageURL(Image image){
