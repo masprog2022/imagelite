83f0f3ac5cb2605f45a33852def5c3e5ccf997b5
masprog2022 <masprog2022@gmail.com>
2024-02-22 17:55:35 +0100

    feat: created initial project backend

---
+++ server/src/test/java/com/masprogtech/imageliteapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masprogtech.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
