f0465b731baeb3449fbffbcb70add0b535e85b91
masprog2022 <masprog2022@gmail.com>
2024-02-22 22:20:42 +0100

    feat: add ImageServiceImpl

--- server/src/main/java/com/masprogtech/imageliteapi/Application.java
+++ server/src/main/java/com/masprogtech/imageliteapi/Application.java
@@ -1,21 +1,15 @@
 package com.masprogtech.imageliteapi;
 
-import com.masprogtech.imageliteapi.domain.entity.Image;
-import com.masprogtech.imageliteapi.domain.enumerated.ImageExtension;
-import com.masprogtech.imageliteapi.infra.repository.ImageRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application {
 
-	@Bean
-	public CommandLineRunner commandLineRunner(@Autowired ImageRepository repository){
+	//@Bean
+/*	public CommandLineRunner commandLineRunner(@Autowired ImageRepository repository){
       return args -> {
 		  Image image = Image.builder()
 				  .extension(ImageExtension.PNG)
@@ -26,7 +20,7 @@ public class Application {
 
 		  repository.save(image);
 	  };
-	}
+	}*/
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- server/src/main/java/com/masprogtech/imageliteapi/application/images/ImageServiceImpl.java
+++ server/src/main/java/com/masprogtech/imageliteapi/application/images/ImageServiceImpl.java
@@ -1,2 +1,21 @@
-package com.masprogtech.imageliteapi.application.images;public class ImageServiceImpl {
+package com.masprogtech.imageliteapi.application.images;
+
+import com.masprogtech.imageliteapi.domain.entity.Image;
+import com.masprogtech.imageliteapi.infra.repository.ImageRepository;
+import com.masprogtech.imageliteapi.service.ImageService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl implements ImageService {
+
+    private final ImageRepository imageRepository;
+
+    @Override
+    @Transactional
+    public Image save(Image image) {
+        return imageRepository.save(image);
+    }
 }
