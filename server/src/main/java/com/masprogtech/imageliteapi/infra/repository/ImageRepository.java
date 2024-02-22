9ed80251e7f4b6f4b56576884e7348974b459eb5
masprog2022 <masprog2022@gmail.com>
2024-02-23 00:34:59 +0100

    feat: add method findByExtensionAndNameOrTagsLike

--- server/src/main/java/com/masprogtech/imageliteapi/infra/repository/ImageRepository.java
+++ server/src/main/java/com/masprogtech/imageliteapi/infra/repository/ImageRepository.java
@@ -1,8 +1,14 @@
 package com.masprogtech.imageliteapi.infra.repository;
 
 import com.masprogtech.imageliteapi.domain.entity.Image;
+import com.masprogtech.imageliteapi.domain.enumerated.ImageExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+import java.util.List;
 
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
+      return findAll();
+    }
 }
