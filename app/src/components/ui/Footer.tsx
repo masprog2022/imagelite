5517faf2bb0699213af7035dfc86e80448252f52
masprog2022 <masprog2022@gmail.com>
2024-02-24 00:03:22 +0100

    feat: create Footer component

---
+++ app/src/components/ui/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer: React.FC = () => {
+  return (
+    <footer className="py-4 mt-8 text-white bg-indigo-950">
+      <div className="container mx-auto text-center">
+        Desenvolvido por masprogtech - 2024
+      </div>
+    </footer>
+  );
+};
