7d8b0ded109cee263118b97343e2ea5722fa2a3e
masprog2022 <masprog2022@gmail.com>
2024-02-24 00:01:06 +0100

    feat: remove PrimeiroComponent from home

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,7 @@
-import { PrimeiroComponent } from "@/components/PrimeiroComponent";
-
 export default function Home() {
   return (
     <main>
       <div>Hello World!</div>
-      <PrimeiroComponent message="Olá, esta messagem estou passando como parametro" />
     </main>
   );
 }
