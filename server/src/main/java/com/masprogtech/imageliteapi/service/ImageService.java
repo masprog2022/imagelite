3730ce181dd9878d7eca857ead6ba2c198b56eeb
masprog2022 <masprog2022@gmail.com>
2024-02-23 01:20:17 +0100

    feat: change geById to getByID and add method search to the ImageService

--- server/src/main/java/com/masprogtech/imageliteapi/service/ImageService.java
+++ server/src/main/java/com/masprogtech/imageliteapi/service/ImageService.java
@@ -1,11 +1,15 @@
 package com.masprogtech.imageliteapi.service;
 
 import com.masprogtech.imageliteapi.domain.entity.Image;
+import com.masprogtech.imageliteapi.domain.enumerated.ImageExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ImageService {
 
     Image save(Image image);
-    Optional<Image> geById(String id);
+    Optional<Image> getById(String id);
+
+    List<Image> search(ImageExtension extension, String query);
 }
