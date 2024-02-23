783b0a790eae0c913b536e40f419a5495c31027d
masprog2022 <masprog2022@gmail.com>
2024-02-23 01:59:59 +0100

    refactor: refactored code ImageRepository

--- server/src/main/java/com/masprogtech/imageliteapi/infra/repository/ImageRepository.java
+++ server/src/main/java/com/masprogtech/imageliteapi/infra/repository/ImageRepository.java
@@ -2,12 +2,18 @@ package com.masprogtech.imageliteapi.infra.repository;
 
 import com.masprogtech.imageliteapi.domain.entity.Image;
 import com.masprogtech.imageliteapi.domain.enumerated.ImageExtension;
+import com.masprogtech.imageliteapi.infra.specs.GenericSpecs;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
+
+import static com.masprogtech.imageliteapi.infra.specs.GenericSpecs.conjunction;
+import static com.masprogtech.imageliteapi.infra.specs.ImageSpec.*;
+import static org.springframework.data.jpa.domain.Specification.anyOf;
+import static org.springframework.data.jpa.domain.Specification.where;
 
 
 public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
@@ -18,24 +24,16 @@ public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecif
     default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
 
         // SELECT * FROM IMAGE WHERE 1 = 1
-        Specification<Image> conjunction = (root, q, criteriaBuilder) -> criteriaBuilder.conjunction();
-        Specification<Image> spec = Specification.where( conjunction );
+        Specification<Image> spec = where(conjunction());
 
         if(extension != null){
             // AND EXTENSION = 'PNG'
-            Specification<Image> extensionEqual = (root, q, cb) ->cb.equal(root.get("extension"), extension);
-            spec = spec.and(extensionEqual);
+            spec = spec.and(extensionEqual(extension));
         }
 
         if(StringUtils.hasText(query)){
-
             // AND (NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY')
-
-            Specification<Image> nameLike = (root, q, cb) -> cb.like(cb.upper(root.get("name")), "%" + query.toUpperCase() + "%");
-            Specification<Image> tagsLike = (root, q, cb) -> cb.like(cb.upper(root.get("tags")), "%" + query.toUpperCase() + "%");
-
-            Specification<Image> nameOrTagsLike = Specification.anyOf(nameLike, tagsLike);
-            spec = spec.and(nameOrTagsLike);
+            spec = spec.and(anyOf(nameLike(query), tagsLike(query)));
         }
 
       return findAll(spec);
