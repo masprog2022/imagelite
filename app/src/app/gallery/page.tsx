efd85afcc7caccfd9d3f1aaf9becfc8badf595b5
masprog2022 <masprog2022@gmail.com>
2024-02-24 00:18:35 +0100

    feat: config absolute path @component

--- app/src/app/gallery/page.tsx
+++ app/src/app/gallery/page.tsx
@@ -1,9 +1,9 @@
-import { Template } from '../../components/Template'
+import { Template } from "@/components";
 
 export default function GalleryPage() {
   return (
     <Template>
       <h1>Galeria</h1>
     </Template>
-  )
+  );
 }
