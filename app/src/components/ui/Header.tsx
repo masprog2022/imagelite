a58d59b047577d060812f0c814897a29016f82fb
masprog2022 <masprog2022@gmail.com>
2024-02-24 00:03:38 +0100

    feat create Header component

---
+++ app/src/components/ui/Header.tsx
@@ -0,0 +1,9 @@
+export const Header: React.FC = () => {
+  return (
+    <header className="py-3 text-white bg-indigo-950">
+      <div className="container flex items-center justify-between px-4 mx-auto">
+        <h1 className="text-3xl font-bold">ImageLite</h1>
+      </div>
+    </header>
+  );
+};
