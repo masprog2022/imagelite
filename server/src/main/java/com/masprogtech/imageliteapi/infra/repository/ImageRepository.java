40dda0c681c8cd4fc21059a1e27ebdebc8922c33
masprog2022 <masprog2022@gmail.com>
2024-02-23 01:02:49 +0100

    feat: add logic to the method findByExtensionAndNameOrTagsLike

--- server/src/main/java/com/masprogtech/imageliteapi/infra/repository/ImageRepository.java
+++ server/src/main/java/com/masprogtech/imageliteapi/infra/repository/ImageRepository.java
@@ -2,13 +2,42 @@ package com.masprogtech.imageliteapi.infra.repository;
 
 import com.masprogtech.imageliteapi.domain.entity.Image;
 import com.masprogtech.imageliteapi.domain.enumerated.ImageExtension;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
+
 public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+
+    /*
+         SELECT * FROM IMAGE WHERE 1 = 1 AND EXTENSION = 'PNG' AND (NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY')
+     */
     default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
-      return findAll();
+
+        // SELECT * FROM IMAGE WHERE 1 = 1
+        Specification<Image> conjunction = (root, q, criteriaBuilder) -> criteriaBuilder.conjunction();
+        Specification<Image> spec = Specification.where( conjunction );
+
+        if(extension != null){
+            // AND EXTENSION = 'PNG'
+            Specification<Image> extensionEqual = (root, q, cb) ->cb.equal(root.get("extension"), extension);
+            spec = spec.and(extensionEqual);
+        }
+
+        if(StringUtils.hasText(query)){
+
+            // AND (NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY')
+
+            Specification<Image> nameLike = (root, q, cb) -> cb.like(cb.upper(root.get("name")), "%" + query.toUpperCase() + "%");
+            Specification<Image> tagsLike = (root, q, cb) -> cb.like(cb.upper(root.get("tags")), "%" + query.toUpperCase() + "%");
+
+            Specification<Image> nameOrTagsLike = Specification.anyOf(nameLike, tagsLike);
+            spec = spec.and(nameOrTagsLike);
+        }
+
+      return findAll(spec);
     }
 }
