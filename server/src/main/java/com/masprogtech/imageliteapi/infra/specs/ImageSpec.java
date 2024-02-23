1ed19ccb143f968974b3646bf69905955d59e637
masprog2022 <masprog2022@gmail.com>
2024-02-23 01:59:32 +0100

    refactor: refactored code ImageSpec

--- server/src/main/java/com/masprogtech/imageliteapi/infra/specs/ImageSpec.java
+++ server/src/main/java/com/masprogtech/imageliteapi/infra/specs/ImageSpec.java
@@ -1,2 +1,23 @@
-package com.masprogtech.imageliteapi.infra.specs;public class ImageSpec {
+package com.masprogtech.imageliteapi.infra.specs;
+
+import com.masprogtech.imageliteapi.domain.entity.Image;
+import com.masprogtech.imageliteapi.domain.enumerated.ImageExtension;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ImageSpec {
+
+    private ImageSpec(){}
+
+    public static Specification<Image> extensionEqual(ImageExtension extension){
+       return (root, q, cb) ->cb.equal(root.get("extension"), extension);
+    }
+
+    public static Specification<Image> nameLike(String name){
+        return (root, q, cb) -> cb.like(cb.upper(root.get("name")), "%" + name.toUpperCase() + "%");
+    }
+
+    public static Specification<Image> tagsLike(String tags){
+        return (root, q, cb) -> cb.like(cb.upper(root.get("tags")), "%" + tags.toUpperCase() + "%");
+    }
+
 }
