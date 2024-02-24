14889dd5fc0c4f754c3bcdbbce62973d8a1bea5c
masprog2022 <masprog2022@gmail.com>
2024-02-24 01:51:58 +0100

    feat: rendering image in GalleryPage

--- app/src/app/gallery/page.tsx
+++ app/src/app/gallery/page.tsx
@@ -1,33 +1,25 @@
+"use client";
 import { ImageCard, Template } from "@/components";
+import { useImageService } from "@/resources/image/image.service";
+import { useState } from "react";
 
 export default function GalleryPage() {
+  const userService = useImageService();
+  const [images, setImages] = useState<Image[]>([]);
+
+  async function searchImages() {
+    const result = await userService.findAll();
+    setImages(result);
+    console.table(result);
+  }
+
   return (
     <Template>
+      <button className="bg-gray-500" onClick={searchImages}>
+        OK
+      </button>
       <section className="grid grid-cols-4 gap-8">
-        <ImageCard
-          name="Montanha"
-          size="10MB"
-          dataUpload="23/02/2024"
-          src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80"
-        />
-        <ImageCard
-          name="Montanha"
-          size="10MB"
-          dataUpload="23/02/2024"
-          src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80"
-        />
-        <ImageCard
-          name="Montanha"
-          size="10MB"
-          dataUpload="23/02/2024"
-          src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80"
-        />
-        <ImageCard
-          name="Montanha"
-          size="10MB"
-          dataUpload="23/02/2024"
-          src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80"
-        />
+        <ImageCard name="Montanha" size="10MB" dataUpload="23/02/2024" src="" />
       </section>
     </Template>
   );
