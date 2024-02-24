e20223caa74edab083f3945c0519af3ff2f844be
masprog2022 <masprog2022@gmail.com>
2024-02-24 02:38:22 +0100

    feat: add input, button to search and add new

--- app/src/app/gallery/page.tsx
+++ app/src/app/gallery/page.tsx
@@ -31,9 +31,26 @@ export default function GalleryPage() {
 
   return (
     <Template>
-      <button className="bg-gray-500" onClick={searchImages}>
-        OK
-      </button>
+      <section className="flex flex-col items-center justify-center my-5">
+        <div className="flex space-x-4">
+          <input
+            type="text"
+            className="px-3 py-2 text-gray-900 border rounded-lg"
+          />
+          <select className="px-4 py-2 text-gray-900 border rounded-lg">
+            <option>All formats</option>
+          </select>
+          <button
+            onClick={searchImages}
+            className="px-4 py-2 text-white bg-blue-500 rounded-lg"
+          >
+            Search
+          </button>
+          <button className="px-4 py-2 text-white bg-yellow-500 rounded-lg">
+            Add New
+          </button>
+        </div>
+      </section>
       <section className="grid grid-cols-4 gap-8">{renderImageCards()}</section>
     </Template>
   );
