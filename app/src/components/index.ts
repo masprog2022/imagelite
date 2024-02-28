c6c8ecd296c84c8076467df3b5fabf32dee8baf7
masprog2022 <masprog2022@gmail.com>
2024-02-29 00:11:32 +0100

    feat: export useNotification

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,4 +1,5 @@
 export { ImageCard } from "./ImageCard";
 export { Template } from "./Template";
 export { Button } from "./button";
-export { InputText } from "./input"
+export { InputText } from "./input";
+export { useNotification } from "./notification";
