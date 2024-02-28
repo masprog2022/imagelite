10a53891ba6cd324bbaf53dca65385d654ca4000
masprog2022 <masprog2022@gmail.com>
2024-02-28 22:39:41 +0100

    feat:  getting file from the form

--- app/src/app/form/page.tsx
+++ app/src/app/form/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { Button, InputText, Template } from "@/components";
+import { RenderIf } from "@/components/ui/RenderIf";
 import { useFormik } from "formik";
 import Link from "next/link";
+import { useState } from "react";
 
 interface FormProps {
   name: string;
@@ -13,12 +15,23 @@ interface FormProps {
 const formsScheme: FormProps = { name: "", tags: "", file: "" };
 
 export default function FormPage() {
+  const [imagePreview, setImagePreview] = useState<string>();
+
   const formmik = useFormik<FormProps>({
     initialValues: formsScheme,
     onSubmit: (dados: FormProps) => {
       console.log(dados);
     },
   });
+
+  function onFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      formmik.setFieldValue("file", file);
+      const imageURL = URL.createObjectURL(file);
+      setImagePreview(imageURL);
+    }
+  }
 
   return (
     <Template>
@@ -53,22 +66,39 @@ export default function FormPage() {
             </label>
             <div className="flex justify-center px-6 py-10 mt-2 border border-dashed rounded-lg-gray-900/25">
               <div className="text-center">
-                <svg
-                  className="w-12 h-12 mx-auto text-gray-300"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <RenderIf condition={!imagePreview}>
+                  <svg
+                    className="w-12 h-12 mx-auto text-gray-300"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </RenderIf>
                 <div className="flex mt-4 text-sm leading-6 text-gray-600">
                   <label className="relative font-semibold text-indigo-600 bg-white rounded-md cursor-pointer">
-                    <span>Click to upload</span>
-                    <input type="file" className="sr-only" />
+                    <RenderIf condition={!imagePreview}>
+                      <span>Click to upload</span>
+                    </RenderIf>
+
+                    <RenderIf condition={!!imagePreview}>
+                      <img
+                        src={imagePreview}
+                        width={250}
+                        className="rounded-md"
+                      />
+                    </RenderIf>
+
+                    <input
+                      onChange={onFileUpload}
+                      type="file"
+                      className="sr-only"
+                    />
                   </label>
                 </div>
               </div>
