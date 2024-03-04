642b15fd5fdc43be840b983aafd5896523e62e83
masprog2022 <masprog2022@gmail.com>
2024-03-04 17:02:16 +0100

    feat: add config generateToken all

--- server/src/main/java/com/masprogtech/imageliteapi/application/jwt/JwtService.java
+++ server/src/main/java/com/masprogtech/imageliteapi/application/jwt/JwtService.java
@@ -2,12 +2,51 @@ package com.masprogtech.imageliteapi.application.jwt;
 
 import com.masprogtech.imageliteapi.domain.entity.User;
 import com.masprogtech.imageliteapi.domain.service.AccessToken;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
 public class JwtService {
 
+    private final SecretKeyGenerator keyGenerator;
+
     public AccessToken generateToken(User user){
-      return new AccessToken("");
+
+        var key = keyGenerator.getKey();
+        var expirationDate = generateExpirationDate();
+        var claims = generateTokenClaims(user);
+
+
+        String token = Jwts
+                .builder()
+                .signWith(key)
+                .subject(user.getEmail())
+                .expiration(expirationDate)
+                .claims(claims)
+                .compact();
+
+
+      return new AccessToken(token);
+    }
+
+    private Date generateExpirationDate(){
+        var expirationMinutes = 60;
+        LocalDateTime now = LocalDateTime.now().plusMinutes(expirationMinutes);
+        return Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private Map<String, Object> generateTokenClaims(User user){
+       Map<String, Object> claims = new HashMap<>();
+       claims.put("name", user.getName());
+       return claims;
     }
 }
