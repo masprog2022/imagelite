c4960e90dbbeca62e3815018106b3493c053acb2
masprog2022 <masprog2022@gmail.com>
2024-03-04 17:31:40 +0100

    feat: create UserDTO

--- server/src/main/java/com/masprogtech/imageliteapi/application/users/UserDTO.java
+++ server/src/main/java/com/masprogtech/imageliteapi/application/users/UserDTO.java
@@ -1,4 +1,10 @@
 package com.masprogtech.imageliteapi.application.users;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
+    private String name;
+    private String email;
+    private String password;
 }
--- server/src/main/java/com/masprogtech/imageliteapi/application/users/UserMapper.java
+++ server/src/main/java/com/masprogtech/imageliteapi/application/users/UserMapper.java
@@ -1,4 +1,16 @@
 package com.masprogtech.imageliteapi.application.users;
 
+import com.masprogtech.imageliteapi.domain.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+
+    public User mapToUser(UserDTO dto){
+        return  User.builder()
+                .email(dto.getEmail())
+                .name(dto.getName())
+                .password(dto.getPassword())
+                .build();
+    }
 }
