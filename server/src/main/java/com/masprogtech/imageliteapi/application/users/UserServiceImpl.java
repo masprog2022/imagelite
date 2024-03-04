e2b6a4dc0d1e73a6257eed767673867d346992d3
masprog2022 <masprog2022@gmail.com>
2024-03-04 15:24:24 +0100

    feat: add encodePassword

--- server/src/main/java/com/masprogtech/imageliteapi/application/users/UserServiceImpl.java
+++ server/src/main/java/com/masprogtech/imageliteapi/application/users/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.masprogtech.imageliteapi.application.users;
 
+import com.masprogtech.imageliteapi.application.jwt.JwtService;
 import com.masprogtech.imageliteapi.domain.entity.User;
 import com.masprogtech.imageliteapi.domain.exception.DuplicatedTupleException;
 import com.masprogtech.imageliteapi.domain.service.AccessToken;
 import com.masprogtech.imageliteapi.infra.repository.UserRepository;
 import com.masprogtech.imageliteapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
 
     @Override
@@ -28,11 +32,27 @@ public class UserServiceImpl implements UserService {
         if(possibleUser != null){
            throw new DuplicatedTupleException("User already exists!");
         }
+        encodePassword(user);
         return userRepository.save(user);
     }
 
     @Override
     public AccessToken authenticate(String email, String password) {
+        var user = getByEmail(email);
+        if(user == null){
+            return null;
+        }
+
+        boolean matches = passwordEncoder.matches(password, user.getPassword());
+        if(matches){
+            return jwtService.generateToken(user);
+        }
         return null;
+    }
+
+    private void encodePassword(User user){
+        String newPassword = user.getPassword();
+        String encodePassword = passwordEncoder.encode(newPassword);
+        user.setPassword(encodePassword);
     }
 }
