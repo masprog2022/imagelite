eb6cbf6b8b2c78098b6ce8a7ea24ad224edcf02d
masprog2022 <masprog2022@gmail.com>
2024-03-05 17:24:31 +0100

    feat: add type in the InputTextProps

--- app/src/components/input/InputText.tsx
+++ app/src/components/input/InputText.tsx
@@ -4,15 +4,17 @@ interface InputTextProps {
   value?: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
+  type?: string;
 }
 
 export const InputText: React.FC<InputTextProps> = ({
   style,
+  type = "text",
   ...rest
 }: InputTextProps) => {
   return (
     <input
-      type="text"
+      type={type}
       {...rest}
       className={`${style} border px-3 py-2 rounded-lg text-gray-900`}
     />
