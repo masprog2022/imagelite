35c390e679f9c039c57b67e261245515ce6a564d
masprog2022 <masprog2022@gmail.com>
2024-03-08 01:29:22 +0100

    feat: new feature in the tailwindcss

--- app/src/components/ui/Footer.tsx
+++ app/src/components/ui/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer: React.FC = () => {
   return (
-    <footer className="py-4 mt-8 text-white bg-indigo-950">
+    <footer className="flex items-center justify-center py-4 mt-auto text-white bg-indigo-950">
       <div className="container mx-auto text-center">
         Desenvolvido por masprogtech - 2024
       </div>
