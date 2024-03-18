3b1af45ccf3f607d0d419e3725cb3f229306fbb5
masprog2022 <masprog2022@gmail.com>
2024-03-18 16:23:20 +0100

    feat: config security change or update

--- server/src/main/java/com/masprogtech/imageliteapi/config/SecurityConfig.java
+++ server/src/main/java/com/masprogtech/imageliteapi/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.masprogtech.imageliteapi.config;
 
+import com.masprogtech.imageliteapi.application.jwt.JwtService;
+import com.masprogtech.imageliteapi.config.filter.JwtFilter;
+import com.masprogtech.imageliteapi.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -17,20 +22,26 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig {
 
     @Bean
+    public JwtFilter jwtFilter(JwtService jwtService, UserService userService){
+       return new JwtFilter(jwtService, userService);
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder(){
      return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors( c -> c.configure(http))
                 .authorizeHttpRequests( auth -> {
                     auth.requestMatchers("/v1/users/**").permitAll();
-
+                    auth.requestMatchers(HttpMethod.GET, "/v1/images/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
--- server/src/main/java/com/masprogtech/imageliteapi/config/filter/JwtFilter.java
+++ server/src/main/java/com/masprogtech/imageliteapi/config/filter/JwtFilter.java
@@ -1,2 +1,81 @@
-package com.masprogtech.imageliteapi.config.filter;public class JwtFilter {
+package com.masprogtech.imageliteapi.config.filter;
+
+import com.masprogtech.imageliteapi.application.jwt.InvalidTokenException;
+import com.masprogtech.imageliteapi.application.jwt.JwtService;
+import com.masprogtech.imageliteapi.domain.entity.User;
+import com.masprogtech.imageliteapi.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String token = getToken(request);
+
+        if(token != null){
+            try {
+                String email = jwtService.getEmailFromToken(token);
+                User user = userService.getByEmail(email);
+                setUserAsAuthenticated(user);
+            }catch (InvalidTokenException e){
+              log.error("Invalid Token: {} ", e.getMessage());
+            }catch (Exception e){
+               log.error("Token Validation error: {}", e.getMessage());
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    public void setUserAsAuthenticated(User user){
+        UserDetails userDetails = org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+    }
+
+    private String getToken(HttpServletRequest request){
+       String authHeader =  request.getHeader("Authorization");
+       if(authHeader != null){
+           String[] authHeaderParts = authHeader.split(" ");
+           if(authHeaderParts.length == 2){
+               return authHeaderParts[1];
+           }
+       }
+       return null;
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException{
+        return request.getRequestURI().contains("/v1/users");
+    }
 }
